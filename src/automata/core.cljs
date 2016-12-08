f1f98d2600ee7a69b32bb4e985c450fd916f2968
Michael Zavarella <zavarella.dev@gmail.com>
2016-12-08 17:41:16 -0500

    seeding automaton properly now

--- src/automata/core.cljs
+++ src/automata/core.cljs
@@ -18,20 +18,29 @@
 ;; Set up
 (defn random-class-div
   []
-  (let [el (dommy/create-element "div")
+  (let [el (dommy/create-element :div)
         cl (if (= 0 (random-binary))
-             "inactive"
-             "active")]
+             :inactive
+             :active)]
     (dommy/add-class! el cl)))
 
-(defn expand-row
+(defn expand-row!
+  "Creates n divs inside of a parent row"
   [row n]
   (doseq [i (range n)]
     (let [el (random-class-div)
-          div (sel1 ".row")]
+          div (sel1 :.row)]
       (dommy/append! div el))))
 
-(expand-row (sel1 ".row") width)
+(defn seed-automata
+  [size]
+  (let [automata (sel1 :div)]
+    (as-> (dommy/create-element :div) row
+      (dommy/add-class! row :row)
+      (dommy/append! automata row)
+      (expand-row! row size))))
+
+(seed-automata 80)
 
 ;;;;;;;;;;;;;;;;;;
 ;; Do the automata
